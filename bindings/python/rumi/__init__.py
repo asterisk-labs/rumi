52d3f801aaff34e868b5cca9516ada83334a13d5
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2026-07-10 11:29:01 -0500

    add high-level write module, slim public API

--- bindings/python/rumi/__init__.py
+++ bindings/python/rumi/__init__.py
@@ -1,16 +1,9 @@
 from importlib.metadata import version
 
-from ._ffi import lib
-from ._read import RumiArray, index_file, parse, read
 from ._header import RumiHeader
-from ._write import Layout, header_bytes, tile, write
+from ._read import parse, read
+from ._write import write
 
 __version__ = version("rumi-eo")
 
-# The OpenZL frame format version.
-OPENZL_VERSION = lib.rumi_openzl_format_version()
-
-__all__ = [
-    "Layout", "OPENZL_VERSION", "RumiArray", "RumiHeader", "__version__",
-    "header_bytes", "index_file", "parse", "read", "tile", "write",
-]
+__all__ = ["RumiHeader", "parse", "read", "write", "__version__"]
